348e2cfba6bc788e32e540df45497958ab8a84cc
only52607 <1361822439@qq.com>
2020-05-19 18:10:10 +0800

    fix:javascript loader

--- app/src/main/java/io/github/mzdluo123/mirai/android/script/JavaScriptHost.kt
+++ app/src/main/java/io/github/mzdluo123/mirai/android/script/JavaScriptHost.kt
@@ -1,6 +1,5 @@
 package io.github.mzdluo123.mirai.android.script
 
-import android.util.Log
 import kotlinx.coroutines.runBlocking
 import net.mamoe.mirai.Bot
 import org.itxtech.miraijs.coreadapter.IPluginLogger
@@ -8,44 +7,6 @@ import org.itxtech.miraijs.coreadapter.JsRuntime
 import java.io.File
 
 class JavaScriptHost(scriptFile: File, configFile: File) : ScriptHost(scriptFile, configFile) {
-    var runtimes = mutableListOf<JsRuntime>()
-
-    override fun onFetchBot(bot: Bot) {
-        if (!config.enable) return
-        val runtime = JsRuntime(scriptFile, JsLogger)
-        runtime.load()
-        runtime.enable(bot)
-        runtimes.add(runtime)
-    }
-
-    override fun onCreate(): ScriptInfo {
-        //只获取信息，enable时再创建运行时
-        val runtime = JsRuntime(scriptFile, JsLogger)
-        runBlocking {
-            runtime.load().join()
-        }
-        Log.e("Js", "loaded")
-        val runtimeInfo = runtime.pluginInfo
-        return ScriptInfo(
-            runtimeInfo.name,
-            runtimeInfo.author,
-            runtimeInfo.version,
-            runtimeInfo.website,
-            scriptFile.length()
-        )
-    }
-
-    override fun onDisable() {
-        runtimes.forEach {
-            it.disable()
-            it.unload()
-        }
-        runtimes.clear()
-    }
-
-    override fun onEnable() {
-
-    }
 
     var JsLogger = object : IPluginLogger {
         override fun debug(str: String) {
@@ -68,6 +29,35 @@ class JavaScriptHost(scriptFile: File, configFile: File) : ScriptHost(scriptFile
             logger(str)
         }
 
+    }
+    var runtime = JsRuntime(scriptFile, JsLogger)
+
+    override fun onFetchBot(bot: Bot) {
+        if (!config.enable) return
+        runtime.attachBot(bot)
+    }
+
+    override fun onCreate(): ScriptInfo {
+        //只获取信息，enable时再创建运行时
+        runBlocking {
+            runtime.load().join()
+        }
+        val runtimeInfo = runtime.pluginInfo
+        return ScriptInfo(
+            runtimeInfo.name,
+            runtimeInfo.author,
+            runtimeInfo.version,
+            runtimeInfo.website,
+            scriptFile.length()
+        )
+    }
+
+    override fun onDisable() {
+        runtime.disable()
+    }
+
+    override fun onEnable() {
+        runtime.enable()
     }
 
 }
