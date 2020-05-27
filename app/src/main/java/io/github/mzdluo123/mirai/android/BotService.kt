b5db5ef6b37a3a6e47dc13cf7f58719decb46031
mzdluo123 <mzdluo123@foxmail.com>
2020-05-27 18:15:36 +0800

    fix: can't use command

--- app/src/main/java/io/github/mzdluo123/mirai/android/BotService.kt
+++ app/src/main/java/io/github/mzdluo123/mirai/android/BotService.kt
@@ -133,7 +133,12 @@ class BotService : Service(), CommandOwner {
             }
         }
         GlobalScope.launch(handler) { bot.login() }
-
+        bot.subscribeMessages {
+            startsWith("/") { message ->
+                if (bot.checkManager(this.sender.id))
+                    CommandManager.runCommand(ContactCommandSender(bot,this.subject), message)
+            }
+        }
 
         GlobalScope.launch(handler) { sendMessage("$qq login successes") }
         MiraiConsole.frontEnd.pushBot(bot)
