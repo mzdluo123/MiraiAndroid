94cd52fc98b32ad20ede1a2bdb8716c5d1848125
mzdluo123 <mzdluo123@foxmail.com>
2020-06-15 01:13:51 +0800

    Compatible with auto.js

--- app/src/main/java/io/github/mzdluo123/mirai/android/receiver/PushMsgReceiver.kt
+++ app/src/main/java/io/github/mzdluo123/mirai/android/receiver/PushMsgReceiver.kt
@@ -3,7 +3,6 @@ package io.github.mzdluo123.mirai.android.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import io.github.mzdluo123.mirai.android.BotService
 
 class PushMsgReceiver(private val botService: BotService) : BroadcastReceiver() {
@@ -12,8 +11,8 @@ class PushMsgReceiver(private val botService: BotService) : BroadcastReceiver() 
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        val type = intent.getIntExtra("type", 0)
-        val id = intent.getLongExtra("id", 0)
+        val type = intent.getStringExtra("type")?.toInt() ?: return
+        val id = intent.getStringExtra("id")?.toLong() ?: return
         val msg = intent.getStringExtra("msg") ?: return
         when (type) {
             1 -> botService.sendFriendMsg(id, msg)
