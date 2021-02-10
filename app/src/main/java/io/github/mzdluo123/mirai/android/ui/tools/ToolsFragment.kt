3c14948bac5898f07ad12d8938b3818accbbb46e
mzdluo123 <mzdluo123@foxmail.com>
2021-02-10 15:32:02 +0800

    适配新版console的device.json目录

--- app/src/main/java/io/github/mzdluo123/mirai/android/ui/tools/ToolsFragment.kt
+++ app/src/main/java/io/github/mzdluo123/mirai/android/ui/tools/ToolsFragment.kt
@@ -1,12 +1,16 @@
 package io.github.mzdluo123.mirai.android.ui.tools
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import io.github.mzdluo123.mirai.android.R
 import kotlinx.android.synthetic.main.fragment_tools.*
 import splitties.toast.toast
@@ -15,6 +19,7 @@ import java.io.File
 
 class ToolsFragment : Fragment() {
 
+    val viewModel by viewModels<ToolsFragmentViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,15 +28,24 @@ class ToolsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_tools, container, false)
     }
 
-    override fun onStart() {
-        super.onStart()
+    @SuppressLint("SdCardPath")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val adapter =
+            ArrayAdapter(requireContext(), R.layout.item_list_menu, mutableListOf<String>())
+        (menu.editText as AutoCompleteTextView).setAdapter(adapter)
+        viewModel.botList.observe(viewLifecycleOwner, { arrayOfFiles ->
+            adapter.clear()
+            adapter.addAll(arrayOfFiles.map { it.name })
+            adapter.notifyDataSetChanged()
+        })
 
         btn_export_device.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             val uri = FileProvider.getUriForFile(
                 requireContext(),
                 requireContext().getPackageName() + ".provider",
-                File(requireContext().getExternalFilesDir(""), "device.json")
+                getDeviceFile() ?: return@setOnClickListener
             )
             intent.putExtra(Intent.EXTRA_STREAM, uri)
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
@@ -39,8 +53,32 @@ class ToolsFragment : Fragment() {
             requireContext().startActivity(intent)
         }
         btn_reset_device.setOnClickListener {
-            File(requireContext().getExternalFilesDir(""), "device.json").delete()
+            getDeviceFile()?.delete() ?: return@setOnClickListener
             toast("成功")
         }
+//        btn_open_data_folder.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_GET_CONTENT)
+//            intent.addCategory(Intent.CATEGORY_OPENABLE)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//            intent.setDataAndType(
+//                FileProvider.getUriForFile(
+//                    requireContext(),
+//                    requireContext().getPackageName() + ".provider",
+//
+//                ), "file/*"
+//            )
+//            requireContext().startActivity(intent)
+//
+//        }
     }
+
+    private fun getDeviceFile(): File? {
+        val folder = menu.editText?.text
+        if (folder?.isEmpty() != false) {
+            toast("请选择bot")
+            return null
+        }
+        return File(requireContext().getExternalFilesDir(""), "bots/${folder}/device.json")
+    }
+
 }
---
+++ app/src/main/java/io/github/mzdluo123/mirai/android/ui/tools/ToolsFragmentViewModel.kt
@@ -0,0 +1,26 @@
+package io.github.mzdluo123.mirai.android.ui.tools
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.github.mzdluo123.mirai.android.BotApplication
+import java.io.File
+
+class ToolsFragmentViewModel : ViewModel() {
+    val botList: MutableLiveData<Array<File>> = MutableLiveData()
+
+    init {
+
+        refreshBotList()
+    }
+
+    fun refreshBotList() {
+        botList.value =
+            File(
+                BotApplication.context.getExternalFilesDir(""),
+                "bots"
+            ).listFiles { dir, _ ->
+                dir.isDirectory
+            }
+    }
+
+}
