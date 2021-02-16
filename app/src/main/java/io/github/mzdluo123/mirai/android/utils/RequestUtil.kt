05bf7968abd47aab82abf9b687bc6d25db90897a
only52607 <1361822439@qq.com>
2021-02-16 22:43:03 +0800

    Create RequestUtil.kt

---
+++ app/src/main/java/io/github/mzdluo123/mirai/android/utils/RequestUtil.kt
@@ -0,0 +1,28 @@
+package io.github.mzdluo123.mirai.android.utils
+
+import okhttp3.*
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+object RequestUtil {
+    suspend fun get(url: String,clientBuilder:OkHttpClient.Builder.()->Unit = {}):String {
+        val client = OkHttpClient.Builder().apply(clientBuilder).build()
+        val request: Request = Request.Builder()
+            .url(url)
+            .get()
+            .build()
+        val call: Call = client.newCall(request)
+        return suspendCoroutine { continuation ->
+            call.enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    continuation.resumeWithException(e)
+                }
+                override fun onResponse(call: Call, response: Response) {
+                    continuation.resume(response.body.toString())
+                }
+            })
+        }
+    }
+}
